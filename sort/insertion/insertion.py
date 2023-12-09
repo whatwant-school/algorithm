a577ff3ed90a1050b52040920bc9958e30a8f5e7
whatwant <whatwant@gmail.com>
2023-12-10 02:31:51 +0900

    Separated pseudocode into a separate file

--- sort/insertion/insertion.py
+++ sort/insertion/insertion.py
@@ -1,13 +1,3 @@
-# for j = 2 to A.length
-#     key = A[j]
-#     // Insert A[j] into the sorted sequence A[1..j-1].
-#     i = j - 1
-#     while i > 0 and A[i] > key
-#         A[i+1] = A[i]
-#         i = i - 1
-#     A[i+1] = key
-
-
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
